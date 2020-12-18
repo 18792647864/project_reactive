781cd1aaee4743c54eb6e31f1f94bbc5e3f94d64
Spancer.Ann <Spancer.Ann@mspbots.ai>
2020-12-18 16:05:08 +0800

    Initial commit

---
+++ src/main/java/club/manhuang/project_reactive/ProjectReactiveApplication.java
@@ -0,0 +1,13 @@
+package club.manhuang.project_reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectReactiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectReactiveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/club/manhuang/project_reactive/ProjectReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package club.manhuang.project_reactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectReactiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
